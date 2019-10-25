b399217b8e204109b5ffc7a8857a59f2098be881
Kaleb Fischer <kalebfischer@flywheel.io>
2019-10-25 13:06:44 -0500

    add tests for str formatting, do not set subject code on Subject object from double-quoted string

--- tests/test_numeric_string_conversion.py
+++ tests/test_numeric_string_conversion.py
@@ -1,12 +1,19 @@
-from util import quote_numeric_string
 import pytest
+
+from util import quote_numeric_string
 
 
 def test_quote_numeric_str():
     assert quote_numeric_string(9860) == '"9860"'
     assert quote_numeric_string('9860') == '"9860"'
+    assert quote_numeric_string('"9860"') == '"9860"'
     assert quote_numeric_string('nine86zero') == 'nine86zero'
     assert quote_numeric_string('9860zero') == '9860zero'
     assert quote_numeric_string('98.60') == '"98.60"'
 
 
+def test_query_strings():
+    assert 'code={}'.format(quote_numeric_string('9860')) == 'code="9860"'
+    assert 'code={}'.format(quote_numeric_string('"9860"')) == 'code="9860"'
+    assert 'code={}'.format(quote_numeric_string('nine86zero')) == 'code=nine86zero'
+    assert 'code={}'.format(quote_numeric_string('98.60')) == 'code="98.60"'
