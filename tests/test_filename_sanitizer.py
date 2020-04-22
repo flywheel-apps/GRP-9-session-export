e68f75ed3cadc17bdfb556112034b42782c94f29
davidparker1 <davidparker@flywheel.io>
2020-04-22 18:20:52 -0400

    improved unit test for sanitize

--- tests/test_filename_sanitizer.py
+++ tests/test_filename_sanitizer.py
@@ -14,8 +14,8 @@ def test_preserve_special():
     assert ensure_filename_safety('._-') == '._-'
 
 def test_remove_special():
-    assert ensure_filename_safety('`~!@#$%^&*()=+,<>/?;:"[{]}\|') == ''
-    assert ensure_filename_safety("'") == ''
+    assert ensure_filename_safety(r"`~!@#$%^&*()=+,<>/?;:'[{]}\|") == ''
+    assert ensure_filename_safety('"') == ''
 
 def test_remove_space():
     assert ensure_filename_safety('abc 123 DEF') == 'abc123DEF'
@@ -23,4 +23,4 @@ def test_remove_space():
 def test_remove_special_preserve_alphanumeric():
     assert ensure_filename_safety('a_1_!_B@2-c3#.4$xyz') == 'a_1__B2-c3.4xyz'
     assert ensure_filename_safety('&a_*1__(B2-)c3=.+4xyz?') == 'a_1__B2-c3.4xyz'
-    assert ensure_filename_safety('/a\_*/1\_ _/B2-\ c3=. "4xyz ]') == 'a_1__B2-c3.4xyz'
+    assert ensure_filename_safety(r"/a\_*/1\_ _/B2-\ c3=. '4xyz ]") == 'a_1__B2-c3.4xyz'
