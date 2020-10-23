43225cb38e9b43b82e4f8275f50792925e64a847
Nate Richman <naterichman@flywheel.io>
2020-10-23 16:55:58 -0500

    MAIN: Reorganization into a class

--- export_report.py
+++ export_report.py
@@ -12,11 +12,79 @@ class DiffRecord:
     def add_record(self, type, msg):
         diff_store = getattr(self, f'{type}_diffs')
 
+class ExportComparison:
+    def __init__(self, source_container, dest_container, fw):
+        self.diffs = DiffRecord()
+        self.queue = Queue()
+        self.s_cont = source_container
+        self.d_cont = dest_container
+        self.fw = fw
+        self.c_type = source_container.container_type
 
-queue = Queue()
-def main(analysis_id, fw):
+        if source_container.container_type != dest_container.container_type:
+            log.error('You have chosen poorly')
+            sys.exit(1)
 
-    analysis = fw.get_analysis(analysis_id)
+        # Other?
+
+    def _compare_children_containers(self, source_finder, dest_finder):
+        
+        dest_ids = {}
+        for cont in dest.iter_find():
+            dest_ids.add(cont.info.origin_id)
+
+        for cont in source.iter_find():
+            if hash_value(cont.id) not in dest_ids:
+                self.diffs.add_record('container', f'{cont.label} not in dest project')
+            self.queue_children(container)
+
+
+    def compare_children_files(self, source, dest):
+        source_files = set([file.hash for file in source])
+        dest_files = set([file.hash for file in dest])
+
+        # Find hashes in left set not in right
+        diff = source_files.difference(dest_files)
+        
+        # Find files corresponding to those hashes
+
+        for file in source:
+            if file.hash in diff:
+                self.diffs.add_record("file", f"file {file.name} not in dest project ")
+        # Return files that are in source, but not in destination
+        
+    def queue_children(source, dest):
+        for child_type in cont.child_types:
+            source_children = getattr(source,child_type)
+            dest_children = getattr(dest,child_type)
+            # flywheel.finder.Finder(), None, or list(flywheel.models.file_entry.FileEntry)
+            queue.put((source_children, dest_children))
+
+    def compare(analysis_id, fw):
+
+        source_container, dest_container = setup(analysis_id, fw)
+
+        queue_children(source_container, dest_container)
+        
+        while not queue.empty():
+            source, dest = queue.get()
+            if not source:
+                # None or empty list
+                continue
+            elif isinstance(source, flywheel.finder.Finder):
+                # Finder of containers
+                compare_children_containers(source, dest)
+                    
+            elif type(source) is list:
+                # Files or Analyses
+                if isinstance(source[0], flywheel.models.analysis_output.AnalysisOutput):
+                    # TODO: compare_analyses ?
+                    pass
+                elif isinstance(source[0], flywheel.models.file_entry.FileEntry):
+                    compare_files(source, dest)
+
+def setup(analysis, fw):
+    analysis = fw.get_analysis(analysis)
 
     source_id = analysis.parent
     source_container_fn = getattr(fw, f"get_{source_id.type}")
@@ -34,57 +102,7 @@ def main(analysis_id, fw):
         )
     )
 
-
-    queue_children(source_container, dest_container)
-    
-    while not queue.empty():
-        source, dest = queue.get()
-        if not source:
-            # None or empty list
-            continue
-        elif isinstance(source, flywheel.finder.Finder):
-            # Finder of containers
-            compare_children_containers(source, dest)
-                
-        elif type(source) is list:
-            # Files
-            result = []
-            result = compare_files(source, dest)
-            if len(result) > 0:
-                log.warning(f'Differences found in {}')
-
-
-def compare_children_containers(source_finder, dest_finder):
-    dest_ids = {}
-    for cont in dest.iter_find():
-        dest_ids.add(cont.info.origin_id)
-
-    for cont in source.iter_find():
-        if hash_value(cont.id) not in dest_ids:
-            add_record('container', f'{cont.label} not in dest project')
-        queue_children(container)
-
-
-def compare_children_files(source, dest):
-    source_files = set([file.hash for file in source])
-    dest_files = set([file.hash for file in dest])
-
-    # Find hashes in left set not in right
-    diff = source_files.difference(dest_files)
-    
-    # Find files corresponding to those hashes
-    file_diff = [file for file in source if file.hash in diff]
-
-    # Return files that are in source, but not in destination
-    return file_diff
-        
-def queue_children(source, dest):
-    for child_type in cont.child_types:
-        source_children = getattr(source,child_type)
-        dest_children = getattr(dest,child_type)
-        # flywheel.finder.Finder(), None, or list(flywheel.models.file_entry.FileEntry)
-        queue.put((source_children, dest_children))
-
+    return source_container, dest_container
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -94,5 +112,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args.analysis, flywheel.Client(args.api_key))
+    fw = flywheel.Client(args.api_key)
+
+    source_container, dest_container = setup(args.analysis, fw)
+
+    comparison = ExportComparison(source_container, dest_container, fw)
+
+    comparison.compare()
+    comparison.report()
 
