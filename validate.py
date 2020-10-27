a758feceac34f952af124380466a68b1eab328ec
Nate Richman <naterichman@flywheel.io>
2020-10-27 08:53:51 -0500

    MAIN: Incorporate comments from review

--- validate.py
+++ validate.py
@@ -1,12 +1,24 @@
 import logging
 import sys
 
+import backoff
+import flywheel
+
 log = logging.getLogger(__name__)
+
+
+def false_if_exc_is_not_found_or_forbidden(exception):
+    if hasattr(exception, "status"):
+        if exception.status in [401, 403, 404, 405, 422]:
+            return False
+    return True
+
+
 # TODO capture run.py's main validation functionality with the enhancement of validating as
 # many things as possible rather than raising immediately so that users are
 # informed of all problems and don't have to iteratively resolve them
 # also, we'll support subject-level export
-def validate_context(gc):
+def validate_context(gc, exc_info=True):
     """
     Validates the gear_context, and export/archive project states to determine
         if the gear can be executed as configured.
@@ -24,7 +36,6 @@ def validate_context(gc):
 
     """
     # Setup
-    errors = []
     fw = gc.client
     export_project, archive_project, destination = None, None, None
 
@@ -33,39 +44,58 @@ def validate_context(gc):
     # At this time no error would be thrown if neither export, or archive project is passed.
     if gc.config.get("export_project"):
         log.info("Looking up export_project")
-        export_project = get_project(fw, gc.config.get("export_project"), errors)
+        try:
+            export_project = get_project(fw, gc.config.get("export_project"))
+        except flywheel.rest.ApiException:
+            if exc_info:
+                log.error(
+                    f"Could not find export project {gc.config.get('export_project')}"
+                )
+            sys.exit(1)
 
     if gc.config.get("archive_project"):
-        archive_project = get_project(fw, gc.config.get("archive_project"), errors)
+        try:
+            archive_project = get_project(fw, gc.config.get("archive_project"))
+        except flywheel.rest.ApiException:
+            if exc_info:
+                log.error(
+                    f"Could not find archive project {gc.config.get('export_project')}"
+                )
+            sys.exit(1)
 
     if export_project is None:
-        log.error("Export project needs to be specified")
+        if exc_info:
+            log.error("Export project needs to be specified")
         sys.exit(1)
 
     #####
     # Check for projet rules
     if gc.config.get("check_gear_rules"):
-        validate_gear_rules(fw, export_project, errors)
+        rules = validate_gear_rules(fw, export_project, errors)
+        if rules:
+            if exc_info:
+                message = f"Aborting Session Export: {proj_name} has ENABLED GEAR RULES and 'check_gear_rules' == True. If you would like to force the export regardless of enabled gear rules re-run.py the gear with 'check_gear_rules' == False. Warning: Doing so may result in undesired behavior."
+                log.error(message)
+            sys.exit(1)
+        else:
+            log.info("No enabled rules were found. Moving on...")
 
     #####
     # Get and validate destination container
-    destination = get_destination(fw, gc.destination.get("id"), errors)
+    try:
+        destination = get_destination(fw, gc.destination.get("id"))
+    except flywheel.rest.ApiException:
+        if exc_info:
+            log.error(f"Could not find destination with id {gc.destination.get('id')}")
+        sys.exit(1)
+    except ValueError as e:
+        if exc_info:
+            log.error(e.args[0])
+        sys.exit(1)
 
     ####
     # Check whether there is work to do
-    need_to_export = check_exported(
-        fw, destination, gc.config.get("force_export"), errors
-    )
-
-    ####
-    # Handle errors
-    if len(errors) > 0:
-        log.error("Validation Errors: ")
-        for err in errors:
-            log.error(err)
-        # raise RuntimeError() ?
-        log.info("Exiting")
-        sys.exit(1)
+    need_to_export = container_needs_export(destination, gc.config)
 
     if not need_to_export:
         log.warning(
@@ -79,13 +109,13 @@ def validate_context(gc):
     return export_project, archive_project, destination
 
 
-# Questions:
-# * Raise or exit on error/warning
-# * How is context.client pointing to destination instance or is the same api key used across instances?
-# * Check the assumption that this is all the same series.
-
-####### TODO: Pretty sure in the following errors will be changed with each function, but I need to check on that.
-def get_project(fw, project_name, errors=[]):
+@backoff.on_exception(
+    backoff.expo,
+    flywheel.rest.ApiException,
+    max_time=300,
+    giveup=false_if_exc_is_not_found_or_forbidden,
+)
+def get_project(fw, project_name):
     """Lookup given project add to errors if needed
 
     Args:
@@ -97,74 +127,73 @@ def get_project(fw, project_name, errors=[]):
         (flywheel.Project or None): Found project or None if not found
     """
     project = None
-    try:
-        project = fw.lookup(project_name)
-        log.info(f"Found Project {project.label}, id {project.id}")
-    except:
-        errors.append(f"Project {project_name} does not exist")
-
+    project = fw.lookup(project_name)
+    log.debug(f"Found Project {project.label}, id {project.id}")
     return project
 
 
+@backoff.on_exception(
+    backoff.expo,
+    flywheel.rest.ApiException,
+    max_time=300,
+    giveup=false_if_exc_is_not_found_or_forbidden,
+)
 def get_destination(fw, dest_id, errors=[]):
     """Get export destination container
 
     Args:
         fw (flywheel.Client): Flywheel Client
         dest_id (str): Destination id
-        errors (list, optional): List of error messages. Defaults to [].
 
     Returns:
         (flywheel.Subject or flywheel.Session): Destination container, either session or subject
     """
-    dest_analysis, dest_container = (None, None)
-    try:
-        # Destination will be analysis since this is a gear run.
-        #   Find parent to get export destination container
-        dest_analysis = fw.get_analysis(dest_id)
-        dest_container = fw.get(dest_analysis.parent.id)
-        log.debug(f"Found destination container id {dest_container.id}")
-    except:
-        errors.append(f"Could not find analysis with id {dest_id}")
-        return dest_container
+    # Destination will be analysis since this is a gear run.
+    #   Find parent to get export destination container
+    dest_analysis = fw.get_analysis(dest_id)
+    dest_container = fw.get(dest_analysis.parent.id)
+    log.debug(f"Found destination container id {dest_container.id}")
 
     if dest_container.container_type not in ["session", "subject"]:
-        errors.append(
+        raise ValueError(
             "Only session and subject level exports are supported at this time!"
         )
 
     return dest_container
 
 
-def validate_gear_rules(fw, proj, errors=[]):
+@backoff.on_exception(
+    backoff.expo,
+    flywheel.rest.ApiException,
+    max_time=300,
+    giveup=false_if_exc_is_not_found_or_forbidden,
+)
+def validate_gear_rules(fw, proj):
     """Validate that there are no enabled gear rules on project
 
     Args:
         fw (flywheel.Client): Flywheel Client
         proj (flywheel.Project): Project to check for gear rules
-        errors (list, optional): List of error messages. Defaults to [].
+    
+    Returns:
+        (bool): True when export should proceed, False if there are enabled gear rules on the export_project
     """
-    try:
-        proj_name = f"{proj.parents.group}/{proj.label}"
-        log.info(f"Checking for enabled gears on {proj_name}...")
-        gear_rules = [x for x in fw.get_project_rules(proj.id) if x.disabled != True]
-    except:
-        errors.append()
-    # Any reason the above is not ... if x.disabled is False ??
+    proj_name = f"{proj.parents.group}/{proj.label}"
+    log.info(f"Checking for enabled gears on {proj_name}...")
+    gear_rules = [x for x in fw.get_project_rules(proj.id) if x.disabled != True]
+
     if any(gear_rules):
-        message = f"Aborting Session Export: {proj_name} has ENABLED GEAR RULES and 'check_gear_rules' == True. If you would like to force the export regardless of enabled gear rules re-run.py the gear with 'check_gear_rules' == False. Warning: Doing so may result in undesired behavior."
-        errors.append(message)
+        return False
     else:
-        log.info("No enabled rules were found. Moving on...")
+        return True
 
 
-def check_exported(fw, dest, force_export=True, errors=[]):
+def container_needs_export(container, config):
     """Check whether given destination container has already been exported
 
     Args:
-        fw (flywheel.Client): flywheel.Client 
-        dest (flywheel.Session or flywheel.Subject): Destination container
-        errors (list, optional): List of error messages. Defaults to [].
+        container (flywheel.Subject or flywheel.Session): Container to check for export
+        config (dict): Gear configuration
 
     Returns:
         (bool): Whether container has been exported or not.  
@@ -172,20 +201,6 @@ def check_exported(fw, dest, force_export=True, errors=[]):
             but force_export is True, then check_export will return True since we still need to export regardless of whether
             or not the container has already been exported
     """
-    # Get full container with so we can get tags on container
-    exported = False
-    try:
-        get_fn = getattr(fw, f"get_{dest.container_type}")
-        full_dest = get_fn(dest.id)
-
-        exported = True if ("exported" in full_dest.get("tags", [])) else False
-    except:
-        errors.append(
-            f"Could not get tags on destination {dest.container_type} container, id: {dest.id}"
-        )
-        # TODO: Return False here?
-
-    if exported and force_export is False:
-        return False
-    else:
-        return True
+    force_export = config.get("force_export", False)
+    exported_tag = bool("EXPORTED" in container.get("tags", list()))
+    return force_export or not exported_tag
