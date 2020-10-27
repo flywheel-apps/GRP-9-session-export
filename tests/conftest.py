78b6bdfbb2c54be67ad4fb6f3983b5abd68df94a
Nate Richman <naterichman@flywheel.io>
2020-10-27 17:00:04 -0500

    MAIN: Move finder fixture to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,3 +35,43 @@ def get_sdk_mock(mocker):
 @pytest.fixture(scope="function")
 def sdk_mock(get_sdk_mock):
     return get_sdk_mock.return_value
+
+
+@pytest.fixture
+def mock_finder(
+    container=None,
+    finder_type="project",
+    methods={"find": [], "find_one": None, "find_first": None},
+):
+    """[summary]
+
+    Args:
+        container (unittest.mock.MagicMock, optional): Existing container mock. Defaults to None
+        finder_type (str, optional): Finder type, one of {project, subject, session.} Defaults to 'project'.
+        methods (dict, optional):  Dictionary of finder methods and corresponding return values. Defaults to:
+            methods = {
+                'find': [],
+                'find_one': None,
+                'find_first': None
+            }
+
+    Returns:
+        (flywheel.Project, flywheel.Subject, or flywheel.Session): Mocked container with finder methods mocked to the given return values
+
+    """
+    finders = {
+        "Project": ["sessions", "subjects", "acquisitions"],
+        "Subject": ["sessions", "acquisitions"],
+        "Session": ["sessions"],
+    }
+    if not container:
+        container_spec = getattr(flywheel, capitalize(finder_type))
+        spec = dir(container_spec)
+        spec.extend(finders[capitalize(finder_type)])
+        container = MagicMock(spec=spec)
+
+    for name, ret in methods.items():
+        finder_method = getattr(getattr(container, finder_type), name)
+        finder_method.return_value = ret
+
+    return container
--- tests/unit_tests/test_export_report.py
+++ tests/unit_tests/test_export_report.py
@@ -8,44 +8,6 @@ from util import hash_value
 import flywheel
 
 
-@pytest.fixture
-def mock_finder(
-    container=None,
-    finder_type="project",
-    methods={"find": [], "find_one": None, "find_first": None},
-):
-    """[summary]
-
-    Args:
-        container (unittest.mock.MagicMock, optional): Existing container mock. Defaults to None
-        finder_type (str, optional): Finder type, one of {project, subject, session.} Defaults to 'project'.
-        methods (dict, optional):  Dictionary of finder methods and corresponding return values. Defaults to:
-            methods = {
-                'find': [],
-                'find_one': None,
-                'find_first': None
-            }
-
-    Returns:
-        (flywheel.Project, flywheel.Subject, or flywheel.Session): Mocked container with finder methods mocked to the given return values
-
-    """
-    finders = {
-        "Project": ["sessions", "subjects", "acquisitions"],
-        "Subject": ["sessions", "acquisitions"],
-        "Session": ["sessions"],
-    }
-    if not container:
-        container_spec = getattr(flywheel, capitalize(finder_type))
-        spec = dir(container_spec)
-        spec.extend(finders[capitalize(finder_type)])
-        container = MagicMock(spec=spec)
-
-    for name, ret in methods.items():
-        finder_method = getattr(getattr(container, finder_type), name)
-        finder_method.return_value = ret
-
-    return container
 
 
 def test_setup(sdk_mock, analysis):
