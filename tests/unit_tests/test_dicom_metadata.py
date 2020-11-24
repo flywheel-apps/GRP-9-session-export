fa858485d45ca4ff0621ccc74396f0357b72244f
Nate Richman <naterichman@flywheel.io>
2020-11-24 13:31:34 -0600

    MAIN: Add testing for get_pydicom_headers and assure output is the same as GRP-3

--- tests/unit_tests/test_dicom_metadata.py
+++ tests/unit_tests/test_dicom_metadata.py
@@ -1,7 +1,10 @@
 import pydicom
+import pytest
 from pydicom.data import get_testdata_files
+import json
 
-from dicom_metadata import assign_type, get_compatible_fw_header
+from pathlib import Path
+from dicom_metadata import assign_type, get_compatible_fw_header, get_pydicom_header
 
 
 def test_assign_type():
@@ -25,3 +28,40 @@ def test_get_compatible_fw_header():
         "AnatomicRegionSequence": [{"CodeValue": "spam\\eggs"}],
     }
     assert get_compatible_fw_header(test_header_dict) == exp_return
+
+
+exclude_tags = [
+    "[Unknown]",
+    "PixelData",
+    "Pixel Data",
+    "[User defined data]",
+    "[Protocol Data Block (compressed)]",
+    "[Histogram tables]",
+    "[Unique image iden]",
+]
+
+
+@pytest.mark.parametrize(
+    "in_file,out_file",
+    [
+        ("MR_small.dcm", "dicom_out_known_good.json"),
+        ("CT_small.dcm", "dicom_out_CT_small.json"),
+    ],
+)
+def test_get_pydicom_header_all_tags(in_file, out_file):
+    test_dicom_path = get_testdata_files(in_file)[0]
+    dcm = pydicom.read_file(test_dicom_path)
+    header = get_pydicom_header(dcm)
+
+    headers_that_we_care_about = [
+        head
+        for head in dcm.dir()
+        if (
+            head not in exclude_tags and not isinstance(head, pydicom.sequence.Sequence)
+        )
+    ]
+    with open(str(Path(__file__).parents[1] / f"data/{out_file}"), "r") as fp:
+        known_good = json.load(fp)
+        for key, val in known_good.items():
+            if val:
+                assert header[key] == val
